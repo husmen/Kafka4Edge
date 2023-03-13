39f2a640807b9ad3be8cca4005927b0d855dec12
Houssem Menhour <husmen93@gmail.com>
2023-03-13 04:06:47 +0200

    add edge node and final data producer

--- README.md
+++ README.md
@@ -3,28 +3,21 @@ Demonstrating Data Offloading for Digital Twins with Kafka - Project for  521290
 
 ## How-to
 
-Run the cluster, remove `-d` to watch logs.
+Run the main and edge clusters:
 
 ```bash
-docker compose up -d --build
+docker compose -f ./docker-compose-main.yml up -d
+docker compose -f ./docker-compose-edge.yml up -d --build --force-recreate
 ```
 
-Then check web api on [http://localhost:8000/docs](http://localhost:8000/docs).
-
-To run outside docker, comment out `producer-0` service in `docker-compose.yml` and run
-
-```bash
-python demo/producer.py
-```
-
-**N.B.** only second method is working for now
+Then check web API on [http://localhost:8000/docs](http://localhost:8000/docs).
 
 ## TODO
 - [x] Kafka cluster configuration
-- [ ] Kafka edge node configuration, to join main cluster
+- [X] Kafka edge node configuration, to join main cluster
 - [x] Simple Producer
 - [ ] Simple Consumer
-- [ ] Multithreaded Producer(s), to simulate multi sensor nodes and larger data loads
+- [X] Multithreaded Producer(s), to simulate multi sensor nodes and larger data loads
 - [ ] Benchmarking, [more info](https://www.ericsson.com/4a492d/assets/local/reports-papers/ericsson-technology-review/docs/2021/xr-and-5g-extended-reality-at-scale-with-time-critical-communication.pdf)
 - [ ] Attach to ditto, maybe?
 
--- demo/producer.py
+++ demo/producer.py
@@ -1,27 +1,48 @@
+import asyncio
+import json
 import logging
-from typing import Union
+import os
+from datetime import datetime
+import socket
 
 import msgpack
-from fastapi import FastAPI
-from pydantic import BaseModel
 import uvicorn
-import random
-
+from fastapi import FastAPI, Path
 from kafka3 import KafkaProducer
 from kafka3.errors import KafkaError
-
-producer: Union[KafkaProducer, None] = None
+from pydantic import BaseModel
+from tqdm import tqdm
 
 FORMAT = "[%(asctime)s] [%(levelname)s] %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
-app = FastAPI(title="Kafka.Producer")
+app = FastAPI(title="Kafka.Producer.DEMO", description="Kafka Producer DEMO", version="0.0.1")
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+class CarData(BaseModel):
+    timestamp: str = ''
+    speed: float
+    coolant_temp: float
+    intake_air_temp: float
+    intake_air_flow_speed: float
+    battery_percentage: float
+    battery_voltage: float
+    current_draw: float
+    engine_vibration_amplitude: float
+    throttle_pos: float
+    tire_pressure11: int
+    tire_pressure12: int
+    tire_pressure21: int
+    tire_pressure22: int
+    accelerometer11_value: float
+    accelerometer12_value: float
+    accelerometer21_value: float
+    accelerometer22_value: float
+    control_unit_firmware: int
+    failure_occurred: bool
 
+
+running_flags: list[bool] = []
+dataset : list[list[list[CarData]]] = []
 
 def on_send_success(record_metadata):
     # Successful result returns assigned partition and offset
@@ -31,37 +52,112 @@ def on_send_error(excp):
     logging.error('MESSAGE ERROR: ', exc_info=excp)
     # handle exception
 
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
+
+async def produce_data(node_id: int, car_id: int, data_point_count: int, data_point_interval: int):
+    global dataset
+
+    try:
+        # configure broker and serializer, msgpack provides better performance than json
+        producer = KafkaProducer(bootstrap_servers=['kafka-0:9092', 'kafka-1:9092', 'kafka-2:9092'], 
+                                client_id=f'{socket.gethostname()}_{node_id}', 
+                                value_serializer=msgpack.dumps, 
+                                acks=1, # default 1
+                                compression_type='lz4', 
+                                retries=1,
+                                batch_size=16384 # default 16KB
+                                )
+
+        for data_point in dataset[node_id][car_id][:data_point_count]:
+            # Asynchronous by default
+            data_point.timestamp = datetime.now().isoformat()
+            producer.send('car_data', {f'car_{car_id}': data_point.dict()}).add_callback(on_send_success).add_errback(on_send_error)
+            await asyncio.sleep(data_point_interval / 1000)
+    except Exception as ex:
+        logging.error('PRODUCE TASK ERROR: ', exc_info=ex)
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.put("/kafka/start/{node_id}/{car_count}/{data_point_count}/{data_point_interval}")
+async def kafka_root(node_id: int = Path(example=0, description='Node ID'), car_count: int = Path(example=5, description='Number of cars'), data_point_count: int = Path(example=10000, description='Number of data points'), data_point_interval: int = Path(example=100, description='Interval between data points in ms')):
+    if running_flags[node_id]:
+        return {"Tasks": "Already Running"}
+    
+    running_flags[node_id] = True
+    async with asyncio.TaskGroup() as tg:
+        tasks = [tg.create_task(produce_data(node_id, car_id, data_point_count, data_point_interval)) for car_id in range(0, car_count)]
+        while not all([t.done() for t in tasks]):
+            await asyncio.sleep(3)
+            if not running_flags[node_id]:
+                _ = [t.cancel() for t in tasks]
+                break
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"Tasks": "Started"}
 
-@app.put("/kafka/{message_count}")
-def update_item(message_count: int):
 
-    # configure broker and serializer, msgpack provides better performance than json
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'], value_serializer=msgpack.dumps, retries=1)
+@app.put("/kafka/stop/{node_id}")
+async def kafka_root(node_id: int = Path(example=0, description='Node ID')):
+    global running_flags
 
-    # Asynchronous by default
-    producer.send('msgpack-topic', {'key': 'value'}).add_callback(on_send_success).add_errback(on_send_error)
+    if running_flags[node_id]:
+        running_flags[node_id] = False
+    
+    return {"Tasks": "Stopped"}
 
-    random.seed(42)
-    for _ in range(message_count):
-        xyz = (random.randint(0, 100), random.randint(0, 100), random.randint(0, 100))
-        msg = {'some-key': b'some value', 'x': xyz[0], 'y': xyz[1], 'z': xyz[2]}
-        producer.send('some-topic', msg).add_callback(on_send_success).add_errback(on_send_error)
 
-    return {"message_count": message_count}
+async def load_data():
+    global dataset
+    global running_flags
+    logging.info('## Loading simulated car sensor data...')
+
+    for (root, dirs, files) in os.walk('data'):
+        for fn in files:
+            if fn.endswith('.txt'):
+                fp = os.path.join(root, fn)
+                with open(fp, 'r') as f:
+                    sensor_data = f.readlines()
+
+                data_points = [None] * len(sensor_data)
+                for i in tqdm(range(len(sensor_data))):
+                    data_points[i] = CarData(**json.loads(sensor_data[i]))
+
+                # data_points = [CarData(**json.loads(data_raw)) for data_raw in sensor_data]
+                
+                car_data = [data_points[i:i + 10000] for i in range(0, len(data_points), 10000)]
+                dataset.append(car_data)
+                running_flags.append(False)
+                
+
+def test_kafka():
+    logging.info("# Kafka producer test")
+    hosts = ['localhost:9092', '127.0.0.1:9092', 'http://127.0.0.1:9092', 'kafka-0:9092', 'http://kafka-0:9092']
+
+    for host in hosts:
+        try:
+            producer = KafkaProducer(bootstrap_servers=[host], client_id='test', value_serializer=msgpack.dumps)
+            logging.info(f"## Kafka broker {host} is available")
+            try:
+                producer.send('test', {'test': 'test'}).add_callback(on_send_success).add_errback(on_send_error)
+                logging.info(f"## Kafka broker {host} is working")
+            except Exception as ex:
+                logging.error('## Kafka produce ERROR: ', exc_info=ex)
+
+            producer.close()
+        except Exception as ex:
+            logging.error(f"## Kafka broker {host} is not available", exc_info=ex)
+
 
 if __name__ == "__main__":
-    logging.info("Kafka producer: app started")
+    logging.info("# Kafka producer demo started")
+    asyncio.run(load_data()) 
+
+    test_kafka()
 
     kwargs = {"host": "0.0.0.0", "port": 80}  # do not use Debug in production
     uvicorn.run(app, **kwargs)
